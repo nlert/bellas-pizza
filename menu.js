db97c92b773fee06cbecbfc990101f876a7201af
nlert <neillertola@gmail.com>
2024-05-03 09:34:27 -0400

    Add menu page styles

--- menu.js
+++ menu.js
@@ -1,32 +1,39 @@
 fetch('menu.json')
-.then(response => response.json())
-.then(data => {
-    for (const category in data.menu) {
-        if (data.menu.hasOwnProperty(category)) {
-            const parentElement = document.getElementById(category);
+	.then((response) => response.json())
+	.then((data) => {
+		for (const category in data.menu) {
+			if (data.menu.hasOwnProperty(category)) {
+				const parentElement = document.getElementById(category);
 
-            data.menu[category].forEach((menuItem) => {
-                if (category === "specialty-pizza") {
-                    return;
-                }
+				data.menu[category].forEach((menuItem) => {
+					const foodRow = document.createElement('div');
+					foodRow.classList.add('food-row');
 
-                const foodRow = document.createElement('div');
-                foodRow.classList.add('food-row');
+					const foodItem = document.createElement('p');
+					foodItem.textContent = menuItem.item;
+					foodRow.appendChild(foodItem);
 
-                const foodItem = document.createElement('p');
-                foodItem.textContent = menuItem.item;
-                foodRow.appendChild(foodItem);
+					const divider = document.createElement('div');
+					divider.classList.add('divider');
+					foodRow.appendChild(divider);
 
-                const divider = document.createElement('div');
-                divider.classList.add('divider');
-                foodRow.appendChild(divider);
+					const itemPrice = document.createElement('p');
 
-                const itemPrice = document.createElement('p');
-                itemPrice.textContent = "$" + menuItem.price.toFixed(2);
-                foodRow.appendChild(itemPrice);
+					if (category === 'specialty-pizza') {
+						itemPrice.textContent = '$' + menuItem.priceLg.toFixed(2);
+					} else {
+						itemPrice.textContent = '$' + menuItem.price.toFixed(2);
+					}
 
-                parentElement.appendChild(foodRow);
-            })
-        }
-    }
-})
+					foodRow.appendChild(itemPrice);
+
+					parentElement.appendChild(foodRow);
+				});
+			}
+		}
+	})
+	.then(() => {
+        document.querySelectorAll('.fade-in').forEach(element => {
+            element.classList.add("show");
+        });
+    });
