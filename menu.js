38097c5f84777b3e40e14583446296f62f83a585
nlert <neillertola@gmail.com>
2024-05-03 10:46:08 -0400

    Render menu categories dynamically

--- menu.js
+++ menu.js
@@ -1,9 +1,20 @@
+const menuElement = document.getElementById('menu');
+
 fetch('menu.json')
 	.then((response) => response.json())
 	.then((data) => {
 		for (const category in data.menu) {
 			if (data.menu.hasOwnProperty(category)) {
-				const parentElement = document.getElementById(category);
+				const parentElement = document.createElement('div');
+                parentElement.id = category;
+
+                parentElement.classList.add('menu-category');
+                parentElement.classList.add('fade-in');
+
+                const heading = document.createElement('h2');
+                heading.textContent = category.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
+                parentElement.appendChild(heading)
 
 				data.menu[category].forEach((menuItem) => {
 					const foodRow = document.createElement('div');
@@ -28,12 +39,19 @@ fetch('menu.json')
 					foodRow.appendChild(itemPrice);
 
 					parentElement.appendChild(foodRow);
+                    menuElement.appendChild(parentElement);
 				});
 			}
 		}
 	})
-	.then(() => {
-        document.querySelectorAll('.fade-in').forEach(element => {
-            element.classList.add("show");
-        });
-    });
+    .then(() => {
+        setTimeout(() => {
+            fadeIn();
+        }, 50);
+    })
+
+function fadeIn() {
+    document.querySelectorAll('.fade-in').forEach(element => {
+        element.classList.add("show");
+    })
+} 
